74a90a4b0bcb75d94f88b038542a70e510a78877
AnnaxKotova <annaxkotova@gmail.com>
2021-07-29 22:34:55 +0200

    fin javascript : ajout messaage d'erreur

--- contacts.js
+++ contacts.js
@@ -25,22 +25,25 @@ document.documentElement.scrollTop = 0;
 //mise en fonction formulaire
 envoyer = document.getElementById("envoyer");
 success = document.getElementById("success");
+rappel = document.getElementById("rappel");
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// Verification de formulaires proposé par Bootstrap
 (function () {
     'use strict'
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    let forms = document.querySelectorAll('.needs-validation')
+    let formulaires = document.querySelectorAll('.needs-validation')
   
     // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
+    Array.prototype.slice.call(formulaires)
+      .forEach
+      (function (form) {
         form.addEventListener('submit', function (event) {
           if (!form.checkValidity()) {
             event.preventDefault() //pour ne pas renouveller la page, mais la vérification aussi disparaît
             event.stopPropagation() //on ne sumbit pas le form si on a les champs qui ne sont pas remplis
-            
+            rappel.classList.remove('none'); //message d'erreur
+            rappel.classList.add('block');
           }
 
           else{
@@ -66,66 +69,3 @@ success = document.getElementById("success");
       success.classList.remove('block');
       success.classList.add('none');
   })
-
-
-// function validate() {
-      
-//     if( document.questionnaire.Name.value == "" ) {
-//        alert( "Please provide your name!" );
-//        document.questionnaire.Name.focus() ;
-//        return false;
-//     }
-//     if( document.questionnaire.EMail.value == "" ) {
-//        alert( "Please provide your Email!" );
-//        document.myForm.EMail.focus() ;
-//        return false;
-//     }
-//     if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
-//        document.myForm.Zip.value.length != 5 ) {
-       
-//        alert( "Please provide a zip in the format #####." );
-//        document.myForm.Zip.focus() ;
-//        return false;
-//     }
-//     if( document.myForm.Country.value == "-1" ) {
-//        alert( "Please provide your country!" );
-//        return false;
-//     }
-//     return( true );
-//  }
-
-// envoyer.addEventListener('click', function(e){
-
-
-// })
-
-
-
-
-
-
-
-// function success(){
-//     if (a){
-//     mybutton.classList.remove('none');
-//      mybutton.classList.add('block');
-//   } else(aa) {
-//     mybutton.classList.add('none');
-//     mybutton.classList.remove('block');
-// }
-
-// message=document.getElementById("envoyer");
-
-// message.addEventListener('click',function(e){
-//     e.preventDefault();
-//     viewPopUp('login', 'typeMessage')
-// })
-
-
-// login.addEventListener('focusout', function(e){
-//     localStorage.setItem('feedback', this.value)
-// })
-
-// if(localStorage.getItem('feedback')){
-//     login.value = localStorage.getItem('feedback');
-// }
