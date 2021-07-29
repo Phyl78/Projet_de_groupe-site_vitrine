2bc373f627b2f0ffdab6c8380cfd0b531137f89c
noemie-hourdeau <85990917+noemie-hourdeau@users.noreply.github.com>
2021-07-29 15:35:24 +0200

    ajout bouton top

--- evenements.js
+++ evenements.js
@@ -39,3 +39,25 @@ function filterSelection(c) {
     });
   }
 }
+
+//back-to-top bouton
+
+mybutton = document.getElementById("backtotop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.classList.remove('none');
+    mybutton.classList.add('block');
+  } else {
+    mybutton.classList.add('none');
+    mybutton.classList.remove('block');
+  }
+}
+
+mybutton.addEventListener('click', function(e){
+
+document.documentElement.scrollTop = 0;
+
+})
